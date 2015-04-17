758241d41c2acd83c8928e66e1a0eabd0e0c55d6
rebornwhu <rebornwhu@gmail.com>
2015-04-16 19:43:42 -0700

    First commit

---
+++ Ch37VowelCounter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Ch37VowelCounter
+//
+//  Created by Xiao Lu on 4/16/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
